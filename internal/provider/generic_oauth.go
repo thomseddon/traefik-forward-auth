bb2349ddf043ef81aaf7c43155a8638c7bc52653
Thom Seddon <thom@seddonmedia.co.uk>
2020-06-28 15:44:24 +0100

    naming fixes

--- internal/provider/generic_oauth.go
+++ internal/provider/generic_oauth.go
@@ -12,14 +12,14 @@ import (
 
 // GenericOAuth provider
 type GenericOAuth struct {
-	OAuthProvider
-
-	LoginURL     string   `long:"login-url" env:"LOGIN_URL" description:"Login URL"`
-	TokenURL     string   `long:"token-url" env:"LOGIN_URL" description:"Login URL"`
-	UserURL      string   `long:"user-url" env:"LOGIN_URL" description:"Login URL"`
+	AuthURL      string   `long:"auth-url" env:"AUTH_URL" description:"Auth/Login URL"`
+	TokenURL     string   `long:"token-url" env:"TOKEN_URL" description:"Token URL"`
+	UserURL      string   `long:"user-url" env:"USER_URL" description:"URL used to retrieve user info"`
 	ClientID     string   `long:"client-id" env:"CLIENT_ID" description:"Client ID"`
 	ClientSecret string   `long:"client-secret" env:"CLIENT_SECRET" description:"Client Secret" json:"-"`
 	Scopes       []string `long:"scope" env:"SCOPE" env-delim:"," default:"profile" default:"email" description:"Scopes"`
+
+	OAuthProvider
 }
 
 // Name returns the name of the provider
@@ -30,8 +30,8 @@ func (o *GenericOAuth) Name() string {
 // Setup performs validation and setup
 func (o *GenericOAuth) Setup() error {
 	// Check parmas
-	if o.LoginURL == "" || o.TokenURL == "" || o.UserURL == "" || o.ClientID == "" || o.ClientSecret == "" {
-		return errors.New("providers.generic-oauth.login-url, providers.generic-oauth.token-url, providers.generic-oauth.user-url, providers.generic-oauth.client-id, providers.generic-oauth.client-secret must be set")
+	if o.AuthURL == "" || o.TokenURL == "" || o.UserURL == "" || o.ClientID == "" || o.ClientSecret == "" {
+		return errors.New("providers.generic-oauth.auth-url, providers.generic-oauth.token-url, providers.generic-oauth.user-url, providers.generic-oauth.client-id, providers.generic-oauth.client-secret must be set")
 	}
 
 	// Create oauth2 config
@@ -39,7 +39,7 @@ func (o *GenericOAuth) Setup() error {
 		ClientID:     o.ClientID,
 		ClientSecret: o.ClientSecret,
 		Endpoint: oauth2.Endpoint{
-			AuthURL:  o.LoginURL,
+			AuthURL:  o.AuthURL,
 			TokenURL: o.TokenURL,
 		},
 		Scopes: o.Scopes,
